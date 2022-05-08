d9f053faea545e9b14d19b6698d28f93b071d40f
KevinNgo14 <kevin.ngo2@mavs.uta.edu>
2022-05-07 19:52:54 -0500

    Project One

---
+++ ProjectOne/src/main/java/Models/Reimbursement.java
@@ -0,0 +1,141 @@
+package Models;
+
+public class Reimbursement {
+	private int id;
+	private int author;
+	private int resolver;
+	private String description;
+	//type
+	private reimbursementType type;
+	//status
+	private Status status;
+	private Double amount;
+
+	public Reimbursement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reimbursement(int id, int author, int resolver, String description,reimbursementType type, Status status,
+			Double amount) {
+		super();
+		this.id = id;
+		this.author = author;
+		this.resolver = resolver;
+		this.description = description;
+		this.type = type;
+		this.status = status;
+		this.amount = amount;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result + author;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + resolver;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (author != other.author)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (resolver != other.resolver)
+			return false;
+		if (status != other.status)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reimbursement [id=" + id + ", author=" + author + ", resolver=" + resolver + ", description="
+				+ description + ", type=" + type + ", status=" + status + ", amount=" + amount + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
+	}
+
+	public int getResolver() {
+		return resolver;
+	}
+
+	public void setResolver(int resolver) {
+		this.resolver = resolver;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public reimbursementType getType() {
+		return type;
+	}
+
+	public void setType(reimbursementType type) {
+		this.type = type;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+}
